0b4ee3305a70e45921ce375308d6d7bde6de6752
1600307376king <1600307376@qq.com>
2019-12-25 10:55:28 +0800

    up

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 app.config.from_pyfile('./config/setting.py')
 
-
+# http://fw.newlifehealth.cn:8099/pp/?enc=58918e22003003000003
 @app.route('/pp/')
 def hello_world():
-    url = request.url
-    print(url)
-    return render_template('jump.html', url=url)
+    enc_value = request.args.get('enc', '')
+
+    filter_obj = 'http://fw.newlifehealth.cn:8099/pp/?enc=' + str(enc_value)
+    return render_template('jump.html', url=filter_obj)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8099)
